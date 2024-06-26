7b62d6e749104f2861725eff9c149e5a6e482f55
tylering6 <tylering6@gmail.com>
2024-06-26 19:29:02 +0000

    update

--- Initiatives/Initiative-script.js
+++ Initiatives/Initiative-script.js
@@ -13,8 +13,9 @@ function addEnemy() {
 
     if (enemyName && !enemies.some(enemy => enemy.name === enemyName)) {
         const randomNumber = Math.floor(Math.random() * 20) + 1; // Random number between 1 and 20
-        if (randomNumber == 20) {
-            enemyName == ' '+' - CRITICAL'
+         // If the random number is 20, set it to 100 bc crit
+         if (randomNumber === 20) {
+            randomNumber = 100;
         }
         enemies.push({ name: enemyName, number: randomNumber });
         updateList();
